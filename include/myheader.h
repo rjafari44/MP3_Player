cd71da79926635fcd1213fe6f3cab64fdd19f9fe
Radin <radin44@protonmail.com>
2026-01-20 09:47:45 -0800

    cleaned up header

--- include/myheader.h
+++ include/myheader.h
@@ -5,10 +5,10 @@
 #include <Arduino.h>
 
 // Pins
-inline constexpr int MP3_RX{20};
-inline constexpr int MP3_TX{21};
-inline constexpr int BUTTON_TOGGLE{4};
-inline constexpr int BUTTON_SKIP{5};
+constexpr int MP3_RX{20};
+constexpr int MP3_TX{21};
+constexpr int BUTTON_TOGGLE{4};
+constexpr int BUTTON_SKIP{5};
 
 // Objects
 inline HardwareSerial mySerial(1);
